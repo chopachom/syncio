ca353969a6ddfe9084e6d7ce82828b77bb5f59fb
Alexander Petrovich <chopachom@gmail.com>
2013-11-10 23:28:02 +0400

    throw error if function isn't a generator

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = function sync(generator){
+  if(!isGeneratorFunction(generator)){
+    throw new Error('Not a generator function');
+  }
   function resume(res){
     var result = iterator.next(res);
     if(typeof result.value === 'function'){
@@ -9,3 +12,7 @@ module.exports = function sync(generator){
   var continuator = iterator.next().value;
   continuator(resume);
 };
+
+function isGeneratorFunction(obj) {
+  return obj && obj.constructor && obj.constructor.name === 'GeneratorFunction';
+}
