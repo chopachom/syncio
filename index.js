61521d494ea79bce40cbf6d13b8e3d4b58ad34c8
Alexander Petrovich <chopachom@gmail.com>
2013-11-11 23:49:37 +0400

    sync: accept completion callback

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ module.exports = function sync(generator){
   if(!isGeneratorFunction(generator)){
     throw new Error('Not a generator function');
   }
+
   function resume(res){
     var result = iterator.next(res);
+    if(result.done && done){
+      return done(null, result.value);
+    }
     if(typeof result.value === 'function'){
       result.value(resume);
     }
   }
+
+  var done;
   var iterator = generator();
   var continuator = iterator.next().value;
   continuator(resume);
+
+  return function(fn){
+    done = fn;
+  }
 };
 
 function isGeneratorFunction(obj) {
