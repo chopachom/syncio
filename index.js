3bb744fc2a7eb950f2c1ca048007f7bfc2fb64ce
Alexander Petrovich <chopachom@gmail.com>
2013-12-03 23:09:18 +0400

    chore index: reformat

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-module.exports = function sync(generator){
-  if(!isGeneratorFunction(generator)){
+module.exports = function sync (generator) {
+  if (!isGeneratorFunction(generator)) {
     throw new Error('Not a generator function');
   }
 
-  function resume(err, res){
-    if(err){
+  function resume (err, res) {
+    if (err) {
       return iterator.throw(err);
     }
     var result = iterator.next(res);
-    if(result.done && done){
+    if (result.done && done) {
       return done(null, result.value);
     }
-    if(typeof result.value === 'function'){
+    if (typeof result.value === 'function') {
       result.value(resume);
     }
   }
@@ -21,11 +21,11 @@ module.exports = function sync(generator){
   var continuator = iterator.next().value;
   continuator(resume);
 
-  return function(fn){
+  return function (fn) {
     done = fn;
   }
 };
 
-function isGeneratorFunction(obj) {
+function isGeneratorFunction (obj) {
   return obj && obj.constructor && obj.constructor.name === 'GeneratorFunction';
 }
